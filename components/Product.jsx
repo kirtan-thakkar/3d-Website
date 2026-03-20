4b64add172d8fe34ddfd0a689c4dcf4ea9baae7b
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-20 23:14:14 +0530

    Add section required before rendering the 3d model

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,11 +1,25 @@
-
-const Product = ()=>{
-    return(
-        <section id="product">
+const Product = () => {
+  return (
+    <section id="product-viewer">
+      <h2>Take closer look</h2>
+      <div className="controls">
+        <p className="info">MacbookPro 16" in Silver / Space Black</p>
+        <div className="flex-center mt-5 gap-4">
+          <div className="color-control">
+            <div className="bg-neutral-300"></div>
+            <div className="bg-neutral-900"></div>
+          </div>
+          <div className="size-control">
             <div>
-                <h1>Product page</h1>
+              <p>14"</p>
             </div>
-        </section>
-    )
-}
+            <div>
+              <p>16"</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default Product;
