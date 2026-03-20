249fb6728ffdee0c5bd15731b07060b6b53fe9d0
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-20 23:07:08 +0530

    Initialse product.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/navbar";
+import Product from "@/components/Product";
 import { Canvas } from "@react-three/fiber";
 import Image from "next/image";
 
@@ -9,6 +10,7 @@ export default function Home() {
     <div>
       <Navbar />
       <Hero />
+      <Product />
     </div>
   );
 }
---
+++ components/Product.jsx
@@ -0,0 +1,11 @@
+
+const Product = ()=>{
+    return(
+        <section id="product">
+            <div>
+                <h1>Product page</h1>
+            </div>
+        </section>
+    )
+}
+export default Product;
