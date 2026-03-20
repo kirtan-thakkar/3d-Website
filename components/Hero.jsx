8bf955fb67caae071529523d0c928f463d84a41d
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-20 23:03:11 +0530

    Add hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,23 @@
-const Hero = ()=>{
-    return(
-        <div>
-            <h1>Hero Section</h1>
-        </div>
-    )
-}
+import { useEffect, useRef } from "react";
+const Hero = () => {
+  const videoRef = useRef(null);
+
+  useEffect(()=>{
+    if(videoRef.current){
+        videoRef.current.playbackRate=2;
+    }
+  },[])
+
+  return (
+    <section id="hero">
+      <div>
+        <h1>Macbook Pro</h1>
+        <img src="/title.png" alt="title" />
+      </div>
+      <video  ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline></video>
+      <button>Buy</button>
+      <p>From $1599 or $133/mo for 12 months</p>
+    </section>
+  );
+};
 export default Hero;
