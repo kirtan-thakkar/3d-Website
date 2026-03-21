a66e67a68b875e53010eb46ef41c86f2b2e06873
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-21 22:11:30 +0530

    Use clsx to track weather color===#adb5d ? active : bg-neutral-300

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,4 +1,8 @@
+"use client";
+import useMacbookStore from "@/app/store";
+import clsx from "clsx";
 const Product = () => {
+  const {color,setColor,scale,setScale}= useMacbookStore();
   return (
     <section id="product-viewer">
       <h2>Take closer look</h2>
@@ -6,7 +10,7 @@ const Product = () => {
         <p className="info">MacbookPro 16" in Silver / Space Black</p>
         <div className="flex-center mt-5 gap-4">
           <div className="color-control">
-            <div className="bg-neutral-300"></div>
+            <div onClick={()=>setColor("#adb5d")} className={clsx("bg-neutral-300",color==="#adb5d" && "active")}></div>
             <div className="bg-neutral-900"></div>
           </div>
           <div className="size-control">
