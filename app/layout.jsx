0a50439812a09c4b18190f5a4b6e410e4ddf6870
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-22 23:11:11 +0530

    Add gsap-react and react-responsive packages

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,6 @@
-import { DM_Sans } from '@next/font/google';
 import "./globals.css";
 import { ViewTransitions } from "next-view-transitions";
 
-const dmSans = DM_Sans({
-  subsets: ["latin"],
-  variable: "--font-dm-sans",
-  weight: ["400", "500", "700"],
-});
 
 export const metadata = {
   title: "3d Website",
@@ -18,7 +12,7 @@ export default function RootLayout({ children }) {
     <ViewTransitions>
       <html
         lang="en"
-        className={`${dmSans.className} bg-neutral-50 h-full antialiased`}
+        className="bg-neutral-50 h-full antialiased"
       >
         <body className="min-h-full flex flex-col">{children}</body>
       </html>
