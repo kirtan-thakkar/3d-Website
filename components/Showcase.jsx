feb669653e347b89c53db83c2e56a201ddbfe5d7
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-25 16:30:13 +0530

    Complete showcase section

--- components/Showcase.jsx
+++ components/Showcase.jsx
@@ -1,9 +1,82 @@
 "use client";
-const Showcase = ()=>{
-    return(
-        <div>
-            <h1>This the is the showcase Component!</h1>
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import Image from "next/image";
+import { useMediaQuery } from "react-responsive";
+
+const Showcase = () => {
+    const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+    useGSAP(()=>{
+        if(!isTablet){
+            const timeline = gsap.timeline({
+                scrollTrigger:{
+                    trigger:"#showcase",
+                    start:"top top", // applys when the top of the section hits the top of the viewport simple
+                    end:"bottom top", // end when the bottom of the section hits the top of the viewport
+                    scrub:true,
+                    pin:true
+                }
+            })
+            timeline.to(".mask img",{
+                transform:'scale(1.1)'
+            }).to(".content",{opacity:1 ,y:0, ease:"power1.in"})
+             
+        }
+    },[isTablet])
+  return (
+    <section id="showcase">
+      <div className="media">
+        <video src="/videos/game.mp4" autoPlay loop muted playsInline />
+        <div className="mask">
+          <img src="/mask-logo.svg" alt="logo" />
         </div>
-    )
-}
+      </div>
+      <div className="content">
+        <div className="wrapper">
+          <div className="lg:max-w-md">
+            <h2>Rocker Chip</h2>
+
+            <div className="mt-7 space-y-5 pe-10">
+              <p>
+                Introducing{" "}
+                <span className="text-white">
+                  M4, the next generation of Apple silicon
+                </span>
+                . M4 powers
+              </p>
+              <p>
+                It drives Apple Intelligence on iPad Pro, so you can write,
+                create, and accomplish more with ease.All in a design that's
+                unbelievably thin, light, and powerfull.
+              </p>
+              <p>
+                A brand-new display delivers breathtaking precision, color
+                accuracy, and brightness. And a next-gen GPU with hardware
+                accelerated ray tracing brings console-level graphics to your
+                fingertips.
+              </p>
+              <p className="text-primary">
+                {" "}
+                Learn more about Apple Intelligence{" "}
+              </p>
+            </div>
+          </div>
+          <div className="max-w-3xs space-y-14">
+            <div className="space-y-2">
+              <p>Up to</p>
+              <h3>4x faster</h3>
+              <p>pro rendering performance than M2</p>
+            </div>
+            <div className="space-y-2">
+              <p>Up to</p>
+              <h3>1.5x faster</h3>
+              <p>CPU performance than M2</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default Showcase;
