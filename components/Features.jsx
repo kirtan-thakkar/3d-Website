a58c1f4a142699e627896f32cb2b7b19d6596470
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-26 21:20:04 +0530

    Add texture in global-store

--- components/Features.jsx
+++ components/Features.jsx
@@ -2,9 +2,10 @@
 
 const Feature = ()=>{
     return(
-        <div>
-            Feauture Section
-        </div>
+        <section id="features">
+            <h2>See it all in a new light.</h2>
+
+        </section>
     )
 }
 export default Feature;
