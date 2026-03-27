1314d29c1469885b501716bbab6d7bb8f7face26
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-27 12:53:47 +0530

    Add footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,33 @@
-"use client";
+const footerLinks = [
+    { label: "Privacy Policy", link: "#" },
+    { label: "Terms of Use", link: "#" },
+    { label: "Sales Policy", link: "#" },
+    { label: "Legal", link: "#" },
+    { label: "Site Map", link: "#" },
+];
 
-const Footer = ()=>{
-    return(
-        <div>
-            Footer section
-        </div>
+const Footer = () => {
+    return (
+        <footer>
+            <div className="info">
+                <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.</p>
+                <img src="/logo.svg" alt ="Apple logo"/>
+            </div>
+
+            <hr />
+
+            <div className="links">
+                <p>Copyright © 2026 kirtan thakkar Inc. All rights reserved.</p>
+
+                <ul>
+                    {footerLinks.map(({label, link }) => (
+                        <li key={label}>
+                            <a href={link}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </footer>
     )
 }
-export default Footer;
+export default Footer
